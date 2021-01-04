6f611cc06248c45052b93ceca3561cf8077dc1f6
Kynan Stevenson <kynants@gmail.com>
2021-01-04 18:38:00 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<!-- https://stackoverflow.com/questions/4221333/get-the-current-script-file-name -->
+<?php
+echo __FILE__;
+?>
+
+</body>
+</html>
